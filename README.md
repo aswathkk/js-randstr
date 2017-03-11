4a984b12e15d04cef50e992fabc752532caeef92
Aswath K <aswath.sana@gmail.com>
2017-03-11 10:36:16 +0530

    Support for custom charset

--- README.md
+++ README.md
@@ -1,11 +1,28 @@
-# randstr
+# js-randstr
 Simple library to generate random strings
+
+#install
+```sh
+npm install js-randstr --save
+```
+
+#usage
+Using in nodejs
+```javascript
+var randstr = require('js-randstr');
+
+// Some random string of length 6
+var str = randstr(6)
+
+console.log(str);
+
+```
 
 #test
 ```sh
-    # install dev dependencies
-    yarn
+# install dev dependencies
+yarn
 
-    # testing
-    npm test
+# testing
+npm test
 ```
--- test/test.spec.js
+++ test/test.spec.js
@@ -2,9 +2,25 @@ var expect = require('chai').expect;
 
 var randstr = require('../index');
 
-describe('#Random String Generator', function() {
+describe('Random String Generator', function() {
     it('returns a string value', function() {
-        var result = randstr('all', 6);
+        var result = randstr(6);
         expect(result).to.be.a('string');
+    });
+
+    it('returns 32 characters by default', function() {
+        var result = randstr();
+        expect(result).to.be.of.length(32);
+    });
+
+    it('returns string of specified length', function() {
+        var result = randstr(6);
+        expect(result).to.be.of.length(6);
+    });
+
+    it('returns string of custom charset', function() {
+        var str = randstr(5, 'abc');
+        var result = str.search(/[^abc]/);
+        expect(result).to.equal(-1);
     });
 });
